617bc7a34cf1ad0596bdc8f915611d1c4c83e04d
Spektor0153 <fas-@mail.ru>
2019-08-03 01:58:40 +0400

    change_1

--- tree.js
+++ tree.js
@@ -2,33 +2,35 @@
 var path = require('path');
 var fs = require('fs');
 var async = require('async');
+let tree_list, console_path;
 
-var tree_list={files: [], dirs:[]};
-var console_path='./';
 
-if (process.argv.length > 2) {
-	console_path=process.argv[2];
+const init = () => {
+    
+    tree_list={files: [], dirs:[]};
+
+    console_path = process.argv.length > 2 ? console_path=process.argv[2] : console_path='./';
+
+    tree_list.dirs.push(path.parse(console_path).name);
 }
 
-tree_list.dirs.push(path.parse(console_path).name);
 
-function getTree (dirPath, callback) {
+let getTree = (dirPath, callback) => {
 
-    fs.readdir(dirPath, function (err, files) {
+    fs.readdir(dirPath, (err, files) => {
         if (err) return callback(err);
         
-        async.each(files, function (fileName, eachCallback) {
+        async.each(files, (fileName, eachCallback) => {
             
-        	var filePath = path.join(dirPath, fileName);
+        	let filePath = path.join(dirPath, fileName);
         	
-        	
-            fs.stat(filePath, function (err, stat) {
+            fs.stat(filePath, (err, stat) => {
                 if (err) return eachCallback(err);
 
                 if (stat.isDirectory()) {
                 	tree_list.dirs.push(filePath);
                 	
-                    getTree(filePath, function (err, subDirFiles) {
+                    getTree(filePath, (err, subDirFiles) => {
                         if (err) return eachCallback(err);
                         eachCallback(null);
                     });
@@ -40,19 +42,23 @@ function getTree (dirPath, callback) {
                 }
                 
             });
-        }, function (err) {
-            callback(err, tree_list);
-        });
+
+        }, (err) => callback(err, tree_list)
+        );
 
     });
 }
 
 
-getTree(console_path, function (err, tree) {
-	if (err) {console.log(err); return}
-	console.log(tree);
-	console.log(JSON.stringify(tree));
+
+init();
+
+getTree(console_path, (err, tree) => {
+    if (err) {console.log(err); return}
+    console.log(tree);
+    console.log(JSON.stringify(tree));
 });
+
 
 /* start project*/
 // npm install
